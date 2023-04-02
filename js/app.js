90f59556e9e691b941f42dd072d19573318eaf31
Reahana <reahana233@gmail.com>
2023-04-02 17:27:09 +0600

    AI Universal Hub

--- js/app.js
+++ js/app.js
@@ -25,17 +25,6 @@ const displayAIs = tools =>{
             <img src="${tool.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Features</h5>
-                `
-                
-                // const ol = document.getElementById('features-list');
-                //  tool.features.forEach(f=>{
-                //  console.log(f)
-                //      const featureLi = document.createElement('li');  
-                //      featureLi.innerHtml = f;
-                //      ol.appendChild(featureLi); 
-                //  });
-            +    
-    `
             
             <ol>
                     <li>${tool.features[0]}</li>
@@ -74,27 +63,20 @@ const displayAIs = tools =>{
                 <img src="${tool.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Features</h5>
-                    `
-                    
-                    // const ol = document.getElementById('features-list');
-                    //  tool.features.forEach(f=>{
-                    //  console.log(f)
-                    //      const featureLi = document.createElement('li');  
-                    //      featureLi.innerHtml = f;
-                    //      ol.appendChild(featureLi); 
-                    //  });
-                +    
-        `
-                
                 <ol>
                         <li>${tool.features[0]}</li>
                         <li>${tool.features[1]}</li>
                         <li>${tool.features[2]}</li>
                     </ol>
                     </div>
-                <div class="card-footer bg-white">
-                    <h5 class="card-title">${tool.name}</h5>
-                    <small class="">${tool.published_in}</small>
+                    <div class="card-footer bg-white">
+                        <div class="float-start">
+                            <h5 class="card-title ">${tool.name}</h5>
+                            <small class="">${tool.published_in}</small>
+                        </div>
+                        <button onclick="loadAiDetail(${tool.id})" type="button"  class="float-end btn btn-danger-subtle" data-bs-toggle="modal" data-bs-target="#aiDetails">
+                        <i class="text-danger fs-2 fa-regular fa-circle-right "></i>
+                        </button>
                 </div>
             </div>
             
@@ -120,27 +102,27 @@ const displayAIs = tools =>{
         const aiDetails = document.getElementById('aiDetailsBody');
         aiDetails.innerHTML = `
         <div class="row row-cols-1 row-cols-md-2 g-4">
-            <div class="col-md-6">
-                <div class="card mx-2">
+            <div class="col-md-6 ">
+                <div class="card mx-2 bg-danger-subtle">
                     <div class="card-body">
-                        <h5 class="card-title">${tool.description}</h5>
-                        <div class="row row-cols-1 row-cols-md-3 g-2">
+                        <h4 class="card-title">${tool.description}</h5>
+                        <div class="row row-cols-1 row-cols-md-3 g-2 text-center px-3 py-5">
                             <div class="col">
-                                <div class="card">
+                                <div class="card  py-3">
                                     <div class="card-body">
                                     <h5 class="card-title text-success">${tool.pricing[0].price} ${tool.pricing[0].plan}</h5>
                                     </div>
                                 </div>
                             </div>
                             <div class="col">
-                                <div class="card">
+                                <div class="card py-3">
                                     <div class="card-body">
                                     <h5 class="card-title text-warning">${tool.pricing[1].price} ${tool.pricing[1].plan}</h5>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="card">
+                            <div class="col ">
+                                <div class="card py-1">
                                     <div class="card-body">
                                     <h5 class="card-title text-danger">${tool.pricing[2].price} ${tool.pricing[2].plan}</h5>
                                     </div>
@@ -179,11 +161,14 @@ const displayAIs = tools =>{
                 </div>
             </div>
             <div class="col-md-6">
+
                 <div class="card">
-                    <img src="${tool.image_link[0]}" class="img-fluid" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">  </h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                   
+                    <img src="${tool.image_link[0]}" class="p-3 img-fluid" alt="..."> 
+                
+                    <div class="card-body text-center">
+                        <h4 class="card-title">${tool.input_output_examples[0].input}  </h5>
+                        <p class="card-text">${tool.input_output_examples[0].output}</p>
                     </div>
                 </div>
             </div>
